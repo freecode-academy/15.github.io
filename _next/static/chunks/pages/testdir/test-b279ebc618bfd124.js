9c30550de8420f78ce52df07ccb441b3ad9e02ec
Fi1osof <Fi1osof@users.noreply.github.com>
2022-05-19 00:35:23 +0000

    deploy: 620486e09f0f9d3042abd6bcadf6e3ad5e454dbb

--- _next/static/chunks/pages/testdir/test-b279ebc618bfd124.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[128],{7358:function(n,t,u){"use strict";u.r(t),t.default=function(){return"Test dir"}},9755:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/testdir/test",function(){return u(7358)}])}},function(n){n.O(0,[774,888,179],(function(){return t=9755,n(n.s=t);var t}));var t=n.O();_N_E=t}]);
